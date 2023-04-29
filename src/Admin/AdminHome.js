ff3ec7eae2d84e34cdd5e8ded0b7b01e9ac511f9
Durga Chowdary Edara <honeyeadala16@gmail.com>
2023-04-29 13:22:32 -0400

    Added a dynamic form to update questions in the Admin Page

--- src/Admin/AdminHome.js
+++ src/Admin/AdminHome.js
@@ -1,49 +1,104 @@
 import '../Users/Login.css';
-import {useState} from "react"
+import {useState, useEffect, useRef} from "react"
 import React from 'react';
-import { useNavigate } from "react-router-dom";
 
 function AdminHome() {
-    const userName = localStorage["admin"] ? localStorage["admin"] : ""
-    const [inputField , setInputField] = useState({"userName" : userName})
-    const navigate = useNavigate();
-    const [res, setRes] = useState({}) 
-    const [error, setError] = useState("") 
+  useEffect(() => {
+    fetch('http://127.0.0.1:5000/admin/getCategory', {
+      method: 'post',
+      headers: {'Content-Type':'application/json'},
+      body: JSON.stringify({"categoryName" : "EXP1"})
+      }).then((response) => response.json())
+          .then((data) => {
+          if(data["status"] === 200){
+            setFormValues(data["result"]["questions"])
+          }          
+      });
+  }, [])
 
-    const inputsHandler = (e) =>{
-        setInputField({...inputField, [e.target.name]: e.target.value})
-    } 
+  const [formValues, setFormValues] = useState([{ name: ""}])
+  const userName = localStorage["admin"] ? localStorage["admin"] : ""
+  const [category , setCategory] = useState("")
+
+    let handleChange = (i, e) => {
+        let newFormValues = [...formValues];
+        newFormValues[i][e.target.name] = e.target.value;
+        setFormValues(newFormValues);
+      }
     
-    const handleSubmit = () =>{             
-        fetch('http://127.0.0.1:5000/admin/login', {
-            method: 'post',
-            headers: {'Content-Type':'application/json'},
-            body: JSON.stringify(inputField)
-            }).then((response) => response.json())
-                .then((data) => {
-                setRes(data)  
-                if(data["status"] === 404){
-                    setError("Admin doesn't exist! Please Sign up first")
-                }
-                else if(data["status"] === 200){
-                    navigate()
-                }          
-            });
-              
+    let addFormFields = () => {
+        setFormValues([...formValues, { name: ""}])
+      }
+    
+    let removeFormFields = (i) => {
+        let newFormValues = [...formValues];
+        newFormValues.splice(i, 1);
+        setFormValues(newFormValues)
     }
-console.log("userName", userName, localStorage["admin"])
-  return (
-    <div className="form_div">
-        <form action="/" id="loginForm" method="POST">
-        <label htmlFor="userName">User Name:</label>
-        <input type="text" id="userName" name="userName" placeholder="Enter your User Name" required onChange={inputsHandler} value={inputField.userName}/><br/><br/>
-        <label htmlFor="password">Password</label>
-        <input type="password" id="password" name="password" placeholder="Enter your Password" required onChange={inputsHandler} value={inputField.password}/><br/><br/>
-        <br/><input type="button" value="Submit" onClick={handleSubmit}/>
-        </form>
-        {error !== "" && <label>{error}</label>}
+    
+    let handleSubmit = (event) => {
+        event.preventDefault();
+        const inputField = {
+          "categoryName" : category,
+          "questions" : formValues,
+          "admin" : userName
+      }      
+      fetch('http://127.0.0.1:5000/admin/addCategory', {
+          method: 'post',
+          headers: {'Content-Type':'application/json'},
+          body: JSON.stringify(inputField)
+          }).then((response) => response.json())
+              .then((data) => {
+              if(data["status"] === 200){
+                alert ("Successfully Saved!")
+                console.log("Successfully stored")
+              }          
+          });
+    }
+
+    const handleCategoryChange = (e) => {
+      setCategory(e.target.value)
+      fetch('http://127.0.0.1:5000/admin/getCategory', {
+        method: 'post',
+        headers: {'Content-Type':'application/json'},
+        body: JSON.stringify({"categoryName" : e.target.value})
+        }).then((response) => response.json())
+            .then((data) => {
+            if(data["status"] === 200){
+              setFormValues(data["result"]["questions"])
+            }
+            else {
+              setFormValues([{"name" : ""}])
+            }          
+        });
+    } 
+
+    return (
+      <div className="form_div_questions">
+        <form  onSubmit={handleSubmit}>
+        <label htmlFor="class">Experiment Class</label>
+                <select id="experiment" name="exp" onChange={(e) => handleCategoryChange(e)} value={category}>
+                <option value="EXP1">EXP1</option>
+                <option value="EXP2">EXP2</option>
+                </select>
+          {formValues.map((element, index) => (
+            <div className="form-inline" key={index}>
+              <label>Name</label>
+              <input type="text" name="name" value={element.name || ""} onChange={e => handleChange(index, e)} />
+              {
+                index ? 
+                  <button type="button"  className="button remove" onClick={() => removeFormFields(index)}>Remove</button> 
+                : null
+              }
+            </div>
+          ))}
+          <div className="button-section">
+              <button className="button add" type="button" onClick={() => addFormFields()}>Add</button>
+              <button className="button submit" type="submit">Submit</button>
+          </div>
+      </form>
     </div>
-  );
+    )
 }
 
 export default AdminHome;
