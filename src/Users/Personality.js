b1aa85404b2ba1edaa7cb2dd0b0eb1d52f347079
SHAFIA DUDEKULA <shafiadudekula@shafias-air.notrust.clients.wireless.pfw.edu>
2023-05-03 11:45:01 -0400

    task one screens

--- src/Users/Personality.js
+++ src/Users/Personality.js
@@ -1,18 +1,19 @@
 import './Personality.css';
-import {useEffect, useState} from "react"
+import { useEffect, useState } from "react"
 import React from 'react';
+import { Link } from "react-router-dom";
 import { useNavigate } from "react-router-dom";
 
 function Personality() {
 
-    const [inputField , setInputField] = useState({})
+    const [inputField, setInputField] = useState({})
 
     let num = 0
 
     const navigate = useNavigate();
 
-    const [res, setRes] = useState({}) 
-    const [label, setLabel] = useState(false) 
+    const [res, setRes] = useState({})
+    const [label, setLabel] = useState(false)
 
     useEffect(() => {
         localStorage.setItem("previourUrl", window.location.pathname);
@@ -21,68 +22,64 @@ function Personality() {
     function generateRandom(min = 1, max = 4) {
         let difference = max - min;
         let rand = Math.random();
-        rand = Math.floor( rand * difference);
+        rand = Math.floor(rand * difference);
         rand = rand + min;
-    
+
         return rand;
     }
 
-    const inputsHandler = (e) =>{
+    const inputsHandler = (e) => {
         console.log(e.target.name, e.target.value)
-        setInputField({...inputField, [e.target.name]: e.target.value})
-    } 
-    
-    const handleSubmit = () =>{
-        console.log(inputField)   
+        setInputField({ ...inputField, [e.target.name]: e.target.value })
+    }
+
+    const handleSubmit = () => {
+        console.log(inputField)
         num = generateRandom()
         console.log(num)
         fetch('http://127.0.0.1:5000/personality', {
             method: 'post',
-            headers: {'Content-Type':'application/json'},
+            headers: { 'Content-Type': 'application/json' },
             body: JSON.stringify(inputField)
-            }).then(({ response }) => {
-                setRes(response)
-                console.log(res)
-                if (num == 1){
-                    // navigate("/CG")  
-                    navigate("/cgInfo")
-                } 
-                else if(num == 2){
-                    // navigate("/exp1")
-                    navigate("/exponeInfo")
-                }   
-                else{
-                    // navigate("/exp2")
-                    navigate("/exptwoInfo")
-                }        
-            });
-              
+        }).then(({ response }) => {
+            setRes(response)
+            console.log(res)
+            if (num == 1) {
+                // navigate("/CG")  
+                navigate("/cgInfo")
+            }
+            else if (num == 2) {
+                // navigate("/exp1")
+                navigate("/exponeInfo")
+            }
+            else {
+                // navigate("/exp2")
+                navigate("/exptwoInfo")
+            }
+        });
+
     }
-        
 
-  return (
-    <div className = "form_div">
-        <form  id="personalityform" method="POST">
-        <label for="per_qs1" >I like being in leadership positions.</label><br/>
-        <input type="checkbox" id="perqs1" name="perqs1" onChange={inputsHandler} value={"agree"}/>
-        <label for="A"> Agree</label><br/>
-        <input type="checkbox" id="perqs1" name="perqs1"  onChange={inputsHandler} value={"neutral"}/>
-        <label for="N"> Neural</label><br/>
-        <input type="checkbox" id="perqs1" name="perqs1"  onChange={inputsHandler} value={"disagree"}/>
-        <label for="D"> Disagree</label><br/><br/>
+    const QuestionAnswer = ({question}) => {
+        return (
+            <div className="ques-container">
+                <p className="ques-text">{question}</p>
+                <textarea className="ans-text" placeholder="Type your answer here"></textarea>
+            </div>
+        )
+    }
 
-        <label for="per_qs2">I express my opinions strongly.</label><br/>
-        <input type="checkbox" id="perqs2" name="perqs2"  onChange={inputsHandler} value={"agree"}/>
-        <label for="A"> Agree</label><br/>
-        <input type="checkbox" id="perqs2" name="perqs2" onChange={inputsHandler} value={"neutral"}/>
-        <label for="N"> Neural</label><br/>
-        <input type="checkbox" id="perqs2" name="perqs2"  onChange={inputsHandler} value={"disagree"}/>
-        <label for="D"> Disagree</label><br/><br/>
-        <br/><input type="button" value="Submit" onClick={handleSubmit}/>
-        </form>
-        {res!== {} && <label>success</label>}
-    </div>
-  );
+    return (
+        <div className="taskone-content">
+            <div className="taskone-container">
+                <QuestionAnswer question={`Have you ever worked hard towards achieving a goal but ultimately failed?\nHow did your failure affect your motivation to pursue similar goals in the future?`} />
+                <QuestionAnswer question={`Did you become discouraged or more determined to succeed?\nWhat steps did you take to maintain your motivation or get back on track after the setback?`} />
+                <div className="gameone-btn">
+                    <Link to="/exp1" className="gamebtn-text">Start game</Link>
+                </div>
+            </div>
+        </div>
+    );
 
 }
 
